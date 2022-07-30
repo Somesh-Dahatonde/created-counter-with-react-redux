b7b2cb44e2adbaddae62b527458e8de0bc561d16
Somesh Dahatonde <63030834+Somesh-Dahatonde@users.noreply.github.com>
2022-07-30 11:55:21 +0530

    action.js updated

--- src/action/action.js
+++ src/action/action.js
@@ -5,10 +5,4 @@ export const RESET = "RESET";
 export const increment = () => ({ type: INCREMENT });
 export const decrement = () => ({ type: DECREMENT });
 export const reset = () => ({ type: RESET });
-// export const INCREMENT = "INCREMENT";
-// export const DECREMENT = "DECREMENT";
-// export const SET = "SET";
 
-// export const increment = () => ({ type: INCREMENT });
-// export const decrement = () => ({ type: DECREMENT });
-// export const set = (value) => ({ type: SET, payload: value });
